98c4333b7e7b41aa53d8325b8ce759b09b1f752a
Andrej Georgievski <andrej.zgeorgievski@gmail.com>
2018-04-04 02:17:35 +0200

    Added PID code (unfinished)

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,21 +1,39 @@
+#include <cstdio>
 #include "PID.h"
 
 using namespace std;
 
 /*
-* TODO: Complete the PID class.
+* Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID() {
+    p_error = 0;
+    d_error = 0;
+    i_error = 0;
+    previous_d_error = 0;
+
+    Kp = 0.1;
+    Kd = 1;
+    Ki = 0.01;
+}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+    p_error = cte;
+    i_error += cte;
+    d_error = cte - previous_d_error;
+    previous_d_error = cte;
 }
 
 double PID::TotalError() {
+    return -Kp*p_error - Kd*d_error - Ki*i_error;
 }
 
